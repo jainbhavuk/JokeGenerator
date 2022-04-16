a46de6e625bebb8e5859a10c8aa10815f1c42caa
jainbhavuk <bhavukspeaks@gmail.com>
2022-04-17 00:37:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('.mybtn');
+const jokeInput = document.getElementById('joke');
+
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    setJoke();
+})
+
+ async function setJoke()
+{
+    let data = await fetch('https://api.chucknorris.io/jokes/random');
+    let res =  await data.json();
+    console.log(res);
+    console.log(res.value);
+    jokeInput.style.display="block";
+    jokeInput.textContent=res.value;
+}
